1d504c711bf762a5c32a30a0499bfa74ceb426bf
YudiR <yudrose13@gmail.com>
2019-07-23 12:21:36 -0400

    data only gets deleted on x click of user if they are the same user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Name from "./components/name/name";
-import Data from "./components/data/data"
-import Title from './components/title/title'
+import Data from "./components/data/data";
+import Title from "./components/title/title";
 import "./App.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       users: [],
-      data:[]
+      data: []
     };
   }
 
@@ -25,39 +25,52 @@ class App extends React.Component {
       });
   }
 
-delete = index =>{
-  console.log(index)
-  let dataCopy = JSON.parse(JSON.stringify(this.state.users));
+  delete = index => {
+    console.log(index);
+    let dataCopy = JSON.parse(JSON.stringify(this.state.users));
+    console.log(dataCopy[index], "datacopy index:::");
+
+    if (dataCopy[index].id === this.state.data.id) {
+      this.setState({
+        data: []
+      });
+    }
     dataCopy.splice(index, 1);
     this.setState({
-      users: dataCopy,
-      data: []
+      users: dataCopy
     });
-}
+  };
 
   data = index => {
-    console.log("index:::::::" ,index)
+    console.log("index:::::::", index);
     let dataCopy = JSON.parse(JSON.stringify(this.state.users));
 
+    console.log(dataCopy, "dataCopy!");
     this.setState({
       data: dataCopy[index]
-    })
-
-  }
+    });
+  };
 
   render() {
-    if (this.state.data.username)
-   {var ui = <Data {...this.state.data} /> }
-    else {ui = <Title/>}
+    if (this.state.data.username) {
+      var ui = <Data {...this.state.data} />;
+    } else {
+      ui = <Title />;
+    }
 
     return (
       <div>
         {this.state.users.map((user, index) => (
-          <Name key={index} index={index} name={user.name} data={this.data} delete={this.delete}  />
+          <Name
+            key={index}
+            index={index}
+            name={user.name}
+            data={this.data}
+            delete={this.delete}
+          />
         ))}
-        
-{ui}
 
+        {ui}
       </div>
     );
   }
